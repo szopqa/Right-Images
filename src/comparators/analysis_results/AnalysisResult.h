32a07bc3a8e70291331cb0ff78e809dd907243e4
szopqa96@gmail.com <szopqa96@gmail.com>
2019-05-28 23:47:33 +0200

    Removed duplicated files

--- src/comparators/analysis_results/AnalysisResult.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class AnalysisResult
-{
-public:
-    virtual void setResults() = 0;
-    virtual void getResults() = 0;
-};
-
--- src/comparators/image_analyzers/ImageAnalyzer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../representations/Image.h"
-#include "../analysis_results/AnalysisResult.h"
-
-class ImageAnalyzer
-{
-public:
-    // AnalysisResult analyze(Image* img);    
-};
